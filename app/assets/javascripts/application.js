afe5d4638734c744291e68e08f3bab98991c8f37
Miko Taruc <miko.taruc@gmail.com>
2016-02-02 08:09:07 -0500

    backbone install

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,11 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
+//= require underscore
+//= require backbone
+//= require mellon
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
