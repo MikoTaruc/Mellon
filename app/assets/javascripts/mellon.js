9982eef9ab68dd7aa23f3133cd1fc6768ffcd1b8
Miko Taruc <miko.taruc@gmail.com>
2016-02-02 22:07:08 -0500

    initialized backbone js

---
+++ app/assets/javascripts/mellon.js
@@ -0,0 +1,13 @@
+window.Mellon = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Mellon.initialize();
+});
